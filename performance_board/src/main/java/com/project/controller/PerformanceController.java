46a8acdb7b42f1fa33afb4f283676490582aea0f
dlqjsdp <dlqjsdp224@gmail.com>
2025-05-21 12:53:12 +0900

    정보게시판 상세페이지 출력 완료

--- performance_board/src/main/java/com/project/controller/PerformanceController.java
+++ performance_board/src/main/java/com/project/controller/PerformanceController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.project.domain.PerformanceVO;
 import com.project.service.PerformanceService;
@@ -28,8 +29,28 @@ public class PerformanceController {
 	    	System.out.println(">>>>> /performance/performanceList 요청 들어옴");
 	    	
 	        List<PerformanceVO> list = performanceService.fetchPerformances();
+	        list.forEach(vo -> vo.setImgKey(performanceService.extractImgKey(vo.getImage())));
 	        System.out.println("🎯 Controller에서 받은 list 크기: " + list.size());
 	        model.addAttribute("performanceList", list);
 	        return "performance/performanceList";
 	    }
+	    
+	/*    @GetMapping("/performanceGet")
+	    public String getPerformanceDetail(String imgKey, Model model) {
+	        System.out.println("imgKey: " + imgKey);
+	        PerformanceVO performance = performanceService.findByImgKey(imgKey);
+	        System.out.println("performance 객체: " + performance);
+	        model.addAttribute("performance", performance);
+	        return "performance/performanceGet"; 
+	    }*/
+	    
+	    @GetMapping("/performanceGet")
+	    public String getPerformanceDetail(String imgKey, Model model) {
+	        System.out.println("imgKey: " + imgKey);
+	        PerformanceVO performance = performanceService.getOrSavePerformance(imgKey);
+	        System.out.println("performance 객체: " + performance);
+	        model.addAttribute("performance", performance);
+	        return "performance/performanceGet"; 
+	    }
+
 }
